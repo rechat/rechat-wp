722a83ff2247685817f0a17ab3be58ffe5d5a8a4
[majid seyedmiri] <[mseiedmiri@gmail.com]>
2025-11-12 12:02:33 +0330

    fix font size on search2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Rechat Plugin
 Description: Fetches and manages agent, offices, regions, and Listing data from Rechat.
-Version: 5.5.21
+Version: 5.5.22
 Author URI: https://rechat.com/
 Text Domain: rechat-plugin
 License: GPL-2.0-or-later
