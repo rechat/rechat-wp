02504250b297bbb249ff6f94c74aa28b1be2b098
[majid seyedmiri] <[mseiedmiri@gmail.com]>
2026-05-11 00:54:05 +0330

    remove archive folder

--- Archive/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-server-side-render'), 'version' => '52612f820c51ea20f55e');
--- Archive/templates/template-block/rch-offices-block-template.php
+++
@@ -1,5 +0,0 @@
-<li>
-    <a style="background-color:<?php echo esc_attr($regionBgColor); ?>" href="<?php the_permalink(); ?>">
-        <h2 style="color:<?php echo esc_attr($textColor); ?>"><?php the_title(); ?></h2>
-    </a>
-</li>
